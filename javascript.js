9ad63e4c11fd67dd5762e801053712841d1f096f
YMeTTe <45124788+YMeTTe@users.noreply.github.com>
2023-05-18 17:17:11 +0200

    forts.jobb

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-window.alert("Hejsan och välkommen till våran hemsida!")
+let close = document.getElementById("close");
+close.addEventListener("click", myFunction);
+
+function myFunction() {
+    document.getElementById("pop2").style.visibility = "visible";
+    document.getElementById("popup").style.visibility = "hidden";
+}
+let close2 = document.getElementById("pop2");
+close2.addEventListener("click", function(){
+    document.getElementById("pop2").style.visibility = "hidden";
+});
+
+
+
+
